804eee1b4e84753ab1bbe7d402248a9bb5f3330f
equilibrium <equilibrium@192.168.0.178>
2017-06-15 17:26:46 +0200

    Add PostRequest() method

--- src/main/java/com/mycompany/spotifymaven/SpotifyApi.java
+++ src/main/java/com/mycompany/spotifymaven/SpotifyApi.java
@@ -16,6 +16,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
 import java.net.URL;
 
 /**
@@ -25,63 +27,17 @@ import java.net.URL;
 public class SpotifyApi {
 
     public static void main(String[] args) throws IOException {
-        /*
-        System.out.println("hi!");
-        
-        System.out.println(Unirest.post("https://accounts.spotify.com/api/token/{method}")
-                .routeParam("method", "post")
-                .header("Authorization: Basic ", "NTM0NzYyN2JkYzQ0NGEwYzg3ZWI4NGFkZTkwMTc0YzI6NDQ5NjNjMDViY2FjNDlmNGIwZDU1YWE4ZWY5YWY0NDk=")
-                .field("grant_type", "client_credentials")
-                .asJson()
-                .getBody()); 
-         */
- /*
-        URL url = new URL("https://accounts.spotify.com/api/token");
-        HttpURLConnection httpCon = (HttpURLConnection) url.openConnection();
-        String basicAuth = "Basic NTM0NzYyN2JkYzQ0NGEwYzg3ZWI4NGFkZTkwMTc0YzI6NDQ5NjNjMDViY2FjNDlmNGIwZDU1YWE4ZWY5YWY0NDk=";
-        httpCon.setDoOutput(true);
-        httpCon.setRequestMethod("POST");
-        httpCon.setRequestProperty("Authorization", basicAuth);
-        
-        OutputStreamWriter out = new OutputStreamWriter(
-                httpCon.getOutputStream());
-        out.write("grant_type=client_credentials");
-        out.flush();
-        System.out.println(httpCon.getResponseCode());
-        System.out.println(httpCon.getResponseMessage());
-        out.close();
-        
-        
-        String json_response = "";
-        InputStreamReader in = new InputStreamReader(httpCon.getInputStream());
-        BufferedReader br = new BufferedReader(in);
-        String text = "";
-        while ((text = br.readLine()) != null) {
-            json_response += text;
-        }
-        System.out.println(json_response);
 
-        
+        String token = PostRequest();
+        System.out.println("Token: " + token);
         System.out.println("###");
         System.out.println("###");
-        System.out.println("###");
-        
-        
-        JsonObject jsonObject = new JsonParser().parse(json_response).getAsJsonObject();
-        System.out.println(jsonObject.get("access_token").getAsString()); //John
-         */
-
-        System.out.println("###");
-        System.out.println("###");
-        System.out.println("###");
-        System.out.println("###");
-        System.out.println("###");
-        System.out.println("###");
+       
 
         //curl -H "Authorization: Bearer BQA6myy_AB-tyANSfiG2UtWbhHxzplok_i0qZTGGp4N2Lm6oBB6vNQfFa0JhdS7uNgqEC0wzRqAdhDFdWPqyHQ" "https://api.spotify.com/v1/search?q=upc:8033772864862&type=album"
         URL url = new URL("https://api.spotify.com/v1/search?q=upc:8033772864862&type=album");
         HttpURLConnection httpCon = (HttpURLConnection) url.openConnection();
-        String basicAuth = "Bearer BQDmx0sW1jNfqZIqDP6PzqavPrjuX563_nvbJkoFmu-jEG5r6fV9oIJhcLOSkbSnY99STgrhSzAZE2ALYqNqiQ";
+        String basicAuth = "Bearer "+ token;
         httpCon.setRequestMethod("GET");
         httpCon.setRequestProperty("Authorization", basicAuth);
 
@@ -120,6 +76,39 @@ public class SpotifyApi {
         //print result
         System.out.println(response.toString());
 
+    }
+    
+    public static String PostRequest() throws MalformedURLException, ProtocolException, IOException{
+        
+        URL url = new URL("https://accounts.spotify.com/api/token");
+        HttpURLConnection httpCon = (HttpURLConnection) url.openConnection();
+        String basicAuth = "Basic NTM0NzYyN2JkYzQ0NGEwYzg3ZWI4NGFkZTkwMTc0YzI6NDQ5NjNjMDViY2FjNDlmNGIwZDU1YWE4ZWY5YWY0NDk=";
+        httpCon.setDoOutput(true);
+        httpCon.setRequestMethod("POST");
+        httpCon.setRequestProperty("Authorization", basicAuth);
+        
+        OutputStreamWriter out = new OutputStreamWriter(
+                httpCon.getOutputStream());
+        out.write("grant_type=client_credentials");
+        out.flush();
+        //System.out.println(httpCon.getResponseCode());
+        //System.out.println(httpCon.getResponseMessage());
+        out.close();
+        
+        
+        String json_response = "";
+        InputStreamReader in = new InputStreamReader(httpCon.getInputStream());
+        BufferedReader br = new BufferedReader(in);
+        String text = "";
+        while ((text = br.readLine()) != null) {
+            json_response += text;
+        }
+        //System.out.println(json_response);
+        
+        JsonObject jsonObject = new JsonParser().parse(json_response).getAsJsonObject();
+                
+        return jsonObject.get("access_token").getAsString();     
+        
     }
 
 }
